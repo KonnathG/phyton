0ede24009495e0d7f35fffd56d6804ba4e077835
KornelCsik <kornel08@yahoo.com>
2023-11-16 12:22:04 +0100

    L02 labor

--- gyak_08.py
+++ gyak_08.py
@@ -1,25 +1,34 @@
 from tkinter import *
 from tkinter import messagebox
-
 import gyak_09
+from gyak_10 import *
+
+
 def beleptetes_ablak():
     def ok_gomb_feldolgozasa():
+        if ab_jelszokereses(felh_nev.get()) == felh_jelszo.get():
+            messagebox.showinfo("Belépés", "Üdv a fedélzeten!")
+        else:
+            messagebox.showerror("Belépés", "Hibás bejelentkezési adat!")
         belepes.destroy()
-        pass
 
     def reg_gomb_feldolgozasa():
         belepes.destroy()
         pass
 
-    belepes = Tk()
+    belepes = Toplevel()
 
     belepes.title("Felhasználó beléptetése")
 
     felh_nev_cimke = Label(belepes, text="A felhasználó neve (email):")
     felh_jelszo_cimke = Label(belepes, text="Jelszó:")
 
-    felh_nev = Entry(belepes, width=30)
-    felh_jelszo = Entry(belepes, width=20)
+    fhsz = StringVar()
+    fhsz.set("")
+    felh_nev = Entry(belepes, textvariable=fhsz, width=30)
+    fjsz = StringVar()
+    fjsz.set("")
+    felh_jelszo = Entry(belepes, textvariable=fjsz, width=20, show="*")
 
     gomb_ok = Button(belepes, text="OK", command=ok_gomb_feldolgozasa)
     gomb_reg = Button(belepes, text="Regisztráció", command=reg_gomb_feldolgozasa)
@@ -33,21 +42,32 @@ def beleptetes_ablak():
 
     belepes.mainloop()
 
+
 def regisztracio_ablak():
     def ok_gomb_kezelese():
-        if jsz.get() == jsz2.get():
-            regisztracio.destroy()
+        if reg_jelszo.get() != reg_jelszo2.get():
+            messagebox.showerror("Hiba", "Nem egyforma a két jelszó!")
+            regisztracio.attributes('-topmost', True)
+            regisztracio.attributes('-topmost', False)
+        elif fhsz.jelszo_ellenorzese(reg_jelszo.get()):
+            messagebox.showerror("Hiba", "A jelszó nem felel meg a kritériumoknak!")
+            regisztracio.attributes('-topmost', True)
+            regisztracio.attributes('-topmost', False)
+        elif " " in fhemail.get() or "@" not in fhemail.get() or "." not in fhemail.get():
+            messagebox.showerror("Hiba", "Nem megfelelő az email formátum!")
+            regisztracio.attributes('-topmost', True)
+            regisztracio.attributes('-topmost', False)
         else:
-            messagebox.showerror("Hiba", "Nem egyezik a két jelszó")
+            ab_regisztracio(fhemail.get(), reg_jelszo.get())
+            regisztracio.destroy()
+
     def jelszo_gen_gomb_kezelese():
-        pw = gyak_09.Jelszo()
-        pw.jelszo_generalasa(10, True, True, True)
-        jsz0 = pw.jelszo
+        fhsz.jelszo_generalasa(10, True, True, True)
+        jsz0 = fhsz.jelszo
         jsz.set(jsz0)
         jsz2.set(jsz0)
 
-
-    regisztracio = Tk()
+    regisztracio = Toplevel()
     regisztracio.title("Regisztráció")
 
     reg_felh_cimke = Label(regisztracio, text="A felhasználó neve (email):")
@@ -55,15 +75,17 @@ def regisztracio_ablak():
     reg_jelszo_cimke = Label(regisztracio, text="Jelszó:")
     reg_jelszo2_cimke = Label(regisztracio, text="Jelszó ismét:")
 
-    reg_felh = Entry(regisztracio, width=30)
+    fhemail = StringVar()
+    fhemail.set("")
+    reg_felh = Entry(regisztracio, textvariable=fhemail, width=30)
     jsz = StringVar()
     jsz.set("")
     jsz2 = StringVar()
     jsz2.set("")
-    reg_jelszo = Entry(regisztracio, textvariable=jsz, width=20)
-    reg_jelszo2 = Entry(regisztracio, textvariable=jsz2, width=20)
+    reg_jelszo = Entry(regisztracio, textvariable=jsz, width=20, show="*")
+    reg_jelszo2 = Entry(regisztracio, textvariable=jsz2, width=20, show="*")
 
-    gomb_ok = Button(regisztracio, text="OK", command=ok_gomb_kezelese, width=15,)
+    gomb_ok = Button(regisztracio, text="OK", command=ok_gomb_kezelese)
     jelszo_gen_gomb = Button(regisztracio, text="Jelszó generálása", command=jelszo_gen_gomb_kezelese)
 
     reg_felh_cimke.grid(row=0, column=0)
@@ -72,10 +94,33 @@ def regisztracio_ablak():
     reg_felh.grid(row=0, column=1)
     reg_jelszo.grid(row=1, column=1)
     reg_jelszo2.grid(row=2, column=1)
-    gomb_ok.grid(row=3, column=0, columnspan=3, pady=10)
+    gomb_ok.grid(row=3, column=0)
     jelszo_gen_gomb.grid(row=1, column=2)
 
     regisztracio.mainloop()
 
-#beleptetes_ablak()
-regisztracio_ablak()
+
+# főprogram
+app = Tk()
+app.title("Dolgozók nyilvántartása")
+app.minsize(300, 200)
+fhsz = gyak_09.Felhasznalo("", "")
+
+ab_letrehozas()
+
+menusor = Menu(app)
+
+hozzaferes = Menu(menusor)
+hozzaferes.add_command(label="Belépés", command=beleptetes_ablak)
+hozzaferes.add_command(label="Regisztráció", command=regisztracio_ablak)
+hozzaferes.add_command(label="Kilépés", command=app.destroy)
+menusor.add_cascade(label="Hozzáférés", menu=hozzaferes)
+
+dolgozok = Menu(menusor)
+dolgozok.add_command(label="Lista", command=app.destroy)
+dolgozok.add_command(label="Névjegy", command=app.destroy)
+menusor.add_cascade(label="Dolgozók", menu=dolgozok)
+
+app.config(menu=menusor)
+app.mainloop()
+ab_lezaras()
--- gyak_09.py
+++ gyak_09.py
@@ -1,18 +1,45 @@
-class Felhasznalo:
-    pass
-
-
 class Jelszo:
     jelszo = ""
 
-    def __init__(self):
-        pass
+    def __init__(self, jelszo):
+        self.jelszo = jelszo
 
     def jelszo_bekerese(self):
         pass
 
-    def jelszo_ellenorzese(self):
-        pass
+    def jelszo_ellenorzese(self, jelszo):
+        ok_pw = True
+        while ok_pw:
+            if len(jelszo) < 8:
+                ok_pw = False
+
+            van = 0
+            for i in range(len(jelszo)):
+                if jelszo[i].isnumeric():
+                    van += 1
+            if van == 0:
+                ok_pw = False
+
+            van = 0
+            for i in range(len(jelszo)):
+                if jelszo[i].isupper():
+                    van += 1
+            if van == 0:
+                ok_pw = False
+
+            van = 0
+            for i in range(len(jelszo)):
+                if jelszo[i].islower():
+                    van += 1
+            if van == 0:
+                ok_pw = False
+
+            if not ok_pw:
+
+                ok_pw = True
+            else:
+                ok_pw = False
+        return ok_pw
 
     def jelszo_generalasa(self, hossz, nagybetu, kisbetu, szam):
         import string
@@ -29,3 +56,18 @@ class Jelszo:
             jelszo = jelszo + karakterek[random.randint(0, len(karakterek) - 1)]
         self.jelszo = jelszo
         return
+
+
+class Felhasznalo(Jelszo):
+    email = ""
+
+    def __init__(self, email, jelszo):
+        super().__init__(jelszo)
+        self.email = email
+
+
+"""fhsz = Felhasznalo()
+
+fhsz.jelszo
+fhsz.email
+fhsz.jelszo_generalasa()"""
